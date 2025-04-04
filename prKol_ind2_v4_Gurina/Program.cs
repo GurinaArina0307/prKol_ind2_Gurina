0f5b2a0b614561e72519fa925f3bd16fa47d1cea
GurinaArina0307 <gurinaarina227@gmail.com>
2025-04-04 14:32:46 +0500

    Add files via upload

---
+++ prKol_ind2_v4_Gurina/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+
+namespace prKol_ind2_Gurina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "num.txt";
+
+            try
+            {
+                // чтение строк
+                var numbers = File.ReadAllLines(file).Select(line => int.TryParse(line, out int number) ? number : (int?)null).Where(number => number.HasValue).Select(number => number.Value).ToList();
+                // разделение на положительные и отрицательные
+                var polosition = numbers.Where(n => n > 0);
+                var otrication = numbers.Where(n => n < 0);
+
+                // объединение и вывод
+                var result = polosition.Concat(otrication);
+                foreach (var number in result)
+                {
+                    Console.WriteLine(number);
+                }
+
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Ошибка !");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
