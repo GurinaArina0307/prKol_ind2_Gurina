01de6eeed267e5a27bbf4fcb52edf34b32390782
GurinaArina0307 <gurinaarina227@gmail.com>
2025-04-04 15:42:21 +0500

    Add files via upload

---
+++ prKol_ind2_v1_Gurina/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+
+namespace prKol_ind2_v1_Gurina
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string file = "file.txt";
+
+            try
+            {
+                // проверка на наличие файла
+                if (!File.Exists(file))
+                {
+                    MessageBox.Show("Файл не найден!");
+                    return;
+                }
+
+                string fileRead = File.ReadAllText(file);
+                // выбираем все символы отличные от цифр
+                var notNumbers = fileRead.Where(c => !char.IsDigit(c));
+                // выбираем только цифры
+                var numbers = fileRead.Where(c => char.IsDigit(c));
+
+                char[] result = notNumbers.Concat(numbers).ToArray();
+
+                label1.Text = new string(result);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка!");
+            }
+        }
+    }
+}
---
+++ prKol_ind2_v4_Gurina/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+
+namespace prKol_ind2_v4_Gurina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "num.txt";
+            try
+            {
+                // проверка на наличие файла
+                if (!File.Exists(file))
+                {
+                    Console.WriteLine("Файл не найден!");
+                    return;
+                }
+
+                // чтение строк
+                var numbers = File.ReadAllLines(file).Select(line => int.TryParse(line, out int number) ? number : (int?)null).Where(number => number.HasValue).Select(number => number.Value).ToList();
+                // разделение на положительные и отрицательные
+                var polosition = numbers.Where(n => n > 0);
+                var otrication = numbers.Where(n => n < 0);
+
+                // объединение и вывод
+                var result = polosition.Concat(otrication);
+                foreach (var number in result)
+                {
+                    Console.WriteLine(number);
+                }
+
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Ошибка !");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
